18923ee13cfc1f71e617b29e3736602c755c5c8e
GitLab <swapnil.bhosale@incepticeconsulting.com>
2020-06-10 10:14:21 +0530

    Initial Commit

---
+++ Closure/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Closure
+//
+//  Created by Mac on 10/06/20.
+//  Copyright © 2020 Mac. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
